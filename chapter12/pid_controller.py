202a7dfb36ee09cba42695e0bec1ee86c8f4a2d7
Danny Staple <danny@orionrobots.co.uk>
2018-09-25 09:01:42 +0100

    Leave out antiwindup for now.

--- chapter12/pid_controller.py
+++ chapter12/pid_controller.py
@@ -1,17 +1,15 @@
 import math
 
 class PIController(object):
-    def __init__(self, proportional_constant=1.0, integral_constant=0.2, anti_windup=None):
+    def __init__(self, proportional_constant=1.0, integral_constant=0.2):
         self.proportional_constant = proportional_constant
         self.integral_constant = integral_constant
-        self.anti_windup = anti_windup
 
         # Running sums
         self.integral_sum = 0
 
     def get_integral(self, error):
-        if self.anti_windup is None or self.integral_sum < self.anti_windup:
-            self.integral_sum += error
+        self.integral_sum += error
         return self.integral_constant * self.integral_sum
 
     def get_proportional(self, error):
