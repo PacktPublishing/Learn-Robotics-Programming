f3d1a36c158008c044e2b166cd4970c8436f7d8a
Danny Staple <danny@orionrobots.co.uk>
2018-10-31 20:50:25 +0000

    Adding the second intent.

--- chapter14/my-robot-skill/__init__.py
+++ chapter14/my-robot-skill/__init__.py
@@ -11,18 +11,22 @@ class MyRobot(MycroftSkill):
         self.settings.load_skill_settings_from_file()
         self.base_url = self.settings.get("base_url")
 
-
-    @intent_handler(IntentBuilder("").require("robot").require("DriveForward"))
-    def handle_drive_forward(self, message):
+    def handle_start_behavior(self, mode_name):
         try:
-            requests.get(self.base_url + "/run/behavior_line")
+            requests.get(self.base_url + "/run/" + mode_name)
             self.speak_dialog('Robot')
             self.speak_dialog('Starting')
         except:
             self.speak_dialog("UnableToReach")
             LOG.exception("Can't reach the robot")
 
+    @intent_handler(IntentBuilder("").require("robot").require("DriveForward"))
+    def handle_drive_forward(self, message):
+        self.handle_start_behavior("behavior_line")
 
+    @intent_handler(IntentBuilder("").require("robot").require("FollowLine"))
+    def handle_follow_line(self, message):
+        self.handle_start_behavior("line_following")
 
 def create_skill():
     return MyRobot()
